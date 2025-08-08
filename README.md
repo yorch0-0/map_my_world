d22570e4c8c0f3e803e7f7be320af01ff35a8f45
yorch0-0 <jorge.gl0905@gmail.com>
2025-08-08 03:57:31 -0500

    readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # map_my_world
-# map_my_world
+¡Hola! Solo quiero comentarte un par de cosas sobre este proyecto:
+
+- Puedes correr el proyecto con `docker compose up`.
+- El proyecto se encuentra desplegado, puedes acceder a él a través de [este link](https://map-my-world-5910.onrender.com/docs).
+
--- app/main.py
+++ app/main.py
@@ -6,15 +6,17 @@ from app.infraestructure.api.locations.router import router as locations_router
 from app.infraestructure.api.reviews.router import router as reviews_router
 from app.infraestructure.database.config import init_db
 
-app = FastAPI()
+app = FastAPI(title="Map my world")
 
 app.include_router(categories_router)
 app.include_router(locations_router)
 app.include_router(reviews_router)
 
+
 @app.on_event("startup")
 def startup():
     init_db()
+
 
 @app.get('/', tags=['HealthCheck'])
 def health_check():
